f33a7c97eb1404e1eb8bba1b1a366342b73d24d4
akshay chidre <akshaychidre@akshays-MacBook-Pro.local>
2021-02-27 01:15:19 +0530

    Day #2 learn DOM and folder structure, JSX method with example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const name = <span>Hi i am, Akshay Chidre</span>;
+
+// function Test(){
+
+// }
+
+const test =() => {
+  alert("Hello Akshay Chidre");
+}
+
+const age = (a) =>{
+  if(a>18){
+    return("you are not eligiable")
+  }
+  else{
+    return("you are eligiable");
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>{name}</h3>
+        <button onClick={test}>Click Here </button>
+        {age(19)}
       </header>
+      
     </div>
   );
 }
+
+
+// var newElement = document.createElement("h4");
+// newElement.textContent="Hello, i am akshay chidre";
+// document.querySelector("body").appendChild();
 
 export default App;
