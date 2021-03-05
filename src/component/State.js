5b1899a3f36c677b69999497135309cba86f4395
AKSHAYCHIDRE <akshaychidre0@gmail.com>
2021-03-06 00:39:11 +0530

    Day #7 learned how to changed state value in class component

--- src/component/State.js
+++ src/component/State.js
@@ -1,4 +1,70 @@
+// import React from 'react';
+
+// class State extends React.Component{
+
+//     constructor(){
+//         super();
+//         this.state={
+//             msg:'I Have used state method for this component'
+//         }
+//     }
+
+//     click(){
+//         this.setState({
+//             msg:'i have learned state method today'
+//         }
+
+//         )
+
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <h3>{this.state.msg}</h3>
+//                 <button onClick={ () => { this.click() }}>Click Here</button>
+//             </div>
+//         )
+//     }
+// }
+
+
+// export default State;
+
+
+// import React from 'react';
+
+
+// class State extends React.Component{
+
+//     constructor(){
+//         super();
+//         this.state={
+//             msg:"Text Messgae"
+//         }
+//     }
+
+//     click(){
+//         this.setState({
+//             msg:'Changed text Message'
+//         })
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <h3>{this.state.msg} </h3>
+//                 <button onClick={ () => { this.click() }} ></button>
+//             </div>
+//         )
+//     }
+// }
+
+// export default State;
+
+
 import React from 'react';
+
 
 
 
@@ -7,17 +73,27 @@ class State extends React.Component{
     constructor(){
         super();
         this.state={
-            msg:'I have use state method for this component'
-        }        
+            msg:'Text Message'
+        }
     }
-    
+
+    click(){
+        this.setState({
+            msg:'Changed text message'
+        })
+    }
+
     render(){
         return(
             <div>
-                <h1>{this.state.msg}</h1>
+                <h4>
+                    {this.state.msg}
+                </h4>
+                <button onClick={ () => { this.click() } }>Click Here</button>
             </div>
         )
     }
 }
+
 
 export default State;
